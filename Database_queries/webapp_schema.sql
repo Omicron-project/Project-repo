fdff4205f6fb459e348f2b9c73e2ae736756e97e
Kaisa Vuori <vuorikaisalisa@gmail.com>
2025-04-03 09:49:34 +0300

    added raw data create table code to webapp.sql

--- Database_queries/webapp_schema.sql
+++ Database_queries/webapp_schema.sql
@@ -1,4 +1,21 @@
-DROP TABLE IF NOT EXISTS webappdb.ingredients; 
+CREATE TABLE data.water_carbon_combined(
+"FOOD COMMODITY GROUP" VARCHAR,
+"FOOD COMMODITY TYPOLOGY" VARCHAR,
+"FOOD COMMODITY sub-TYPOLOGY" VARCHAR,
+"FOOD COMMODITY ITEM" VARCHAR,
+carbon_mean DECIMAL,
+carbon_median DECIMAL,
+carbon_min DECIMAL,
+carbon_max DECIMAL,
+carbon_suggested_value VARCHAR,
+water_mean DECIMAL,
+water_median DECIMAL,
+water_min DECIMAL,
+water_max DECIMAL,
+water_suggested_value VARCHAR
+);
+
+DROP TABLE IF EXISTS webappdb.ingredients; 
 
 CREATE TABLE IF NOT EXISTS webappdb.ingredients (
 	food_name VARCHAR,
